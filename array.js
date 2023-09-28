9a73ff354599f04e9c4f5ddafa793582276d111b
dayframs <dayframs09@yahoo.com>
2023-09-28 07:13:57 +0100

    second commit

--- array.js
+++ array.js
@@ -1,50 +1,50 @@
-// let arr = [
-//      {
-//          name: 'Ade',
-//          school: 'UI',
-//          dept: 'engineering'
-// },
-// {
-//      name: 'Ola',
-//      school: 'UNILAG',
-//      dept: 'MEDICINE'
-// },
-// {
-//      name: 'Kemi',
-//      school: 'OAU',
-//      dept: 'SCIENCES'
-// }
-//      ]; 
-// function displayArray() {
-//      arrayResult.innerHTML = '';
-//      for (let i = 0; i < arr.length; i++) { 
-//      console.log(arr[i].name);
-//           arrayResult.innerHTML += arr[i].name + ' ' + arr[i].school + ' ' + arr[i].dept + '<br>';
-//           myName.value = '';
-//           school.value = '';
-//           dept.value = '';
-// }
-// }
-// displayArray();
+let arr = [
+     {
+         name: 'Ade',
+         school: 'UI',
+         dept: 'engineering'
+},
+{
+     name: 'Ola',
+     school: 'UNILAG',
+     dept: 'MEDICINE'
+},
+{
+     name: 'Kemi',
+     school: 'OAU',
+     dept: 'SCIENCES'
+}
+     ]; 
+function displayArray() {
+     arrayResult.innerHTML = '';
+     for (let i = 0; i < arr.length; i++) { 
+     console.log(arr[i].name);
+          arrayResult.innerHTML += arr[i].name + ' ' + arr[i].school + ' ' + arr[i].dept + '<br>';
+          myName.value = '';
+          school.value = '';
+          dept.value = '';
+}
+}
+displayArray();
 
-// let obj;
+let obj;
 
-// function addNew() {
-//           let p = myName.value;
-//           let q = school.value;
-//           let r = dept.value;
-//           obj = {
-//                name: p,
-//                school: q,
-//                dept: r
-//           }
-//      arr.push(obj);
-//      displayArray();
-// }
-// function deleteA(){
-//      arr.pop(obj);
-//      displayArray();
-// }
+function addNew() {
+          let p = myName.value;
+          let q = school.value;
+          let r = dept.value;
+          obj = {
+               name: p,
+               school: q,
+               dept: r
+          }
+     arr.push(obj);
+     displayArray();
+}
+function deleteA(){
+     arr.pop(obj);
+     displayArray();
+}
 
 let m;
 let g;
@@ -60,7 +60,7 @@ let array = [mtn, glo, airtel];
 function displayCard() {
      // result.innerHTML = '';
      for (let i = 0; i < number.value; i++){
-          result.innerHTML += company.value + ' ' + amount.value + ' ' + Math.ceil(Math.random() * 100000000) + '<br>';
+          result.innerHTML += company.value + ' ' + '&#8358;'+ amount.value + ' ' + Math.ceil(Math.random() * 100000000) + '<br>';
           console.log(result.innerHTML);
      }
 }    
